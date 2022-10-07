276b53a5f57be90fb028a32ba31220429a0fd5df
samue <samuelsteiner2001@gmail.com>
2022-10-07 09:50:54 +0200

    samuel 20

---
+++ src/Controller/TestingController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TestingController extends AbstractController
+{
+    #[Route('/testing', name: 'app_testing')]
+    public function index(): Response
+    {
+        $test = "0";
+        return $this->render('testing/index.html.twig', [
+            'controller_name' => 'TestingController',
+        ]);
+    }
+}
